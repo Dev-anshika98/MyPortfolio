8c19ba6e8074d276df23683b4a41fdbba75bd0f9
Dev-anshika98 <g.anshikacreative@gmail.com>
2024-04-20 15:21:33 +0530

    intial commit

---
+++ src/components/Home/Type.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Frontend Developer",
+          "Designer",
+          "DevOps Inthusiast",
+          "Open Source Contributor",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
